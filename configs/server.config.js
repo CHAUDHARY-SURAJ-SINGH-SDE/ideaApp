927eb27db92ba49b83b0921c6ae477201db5c3ba
SURAJ SINGH <justsurajsingh3270@gmail.com>
2023-07-19 18:20:29 +0530

    adding server config

---
+++ configs/server.config.js
@@ -0,0 +1,3 @@
+module.exports={
+PORT:7777//intialiazing the port number 
+}//making port visible to all in the module
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');//getting express
+const serverConfig = require('./configs/server.config')
 const app = express();//bring express in use (where app variable is a central charator and act like security guard)
-app.listen(3333,()=>{
-    console.log("server is started");
+app.listen(serverConfig.PORT,()=>{
+    console.log('server is started on the port number :: '+ serverConfig.PORT);
 }); //starting the server at port number 3333 and invoke a anonymous arrow function to print 'server is strted' 
